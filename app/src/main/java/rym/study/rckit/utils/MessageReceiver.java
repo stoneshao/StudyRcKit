26db263702d3ef306a72d89c84bc9ed3806c61a3
zhanglei <zhanglei1@rongcloud.cn>
2016-09-22 15:54:08 +0800

    Add notification click event.

---
+++ app/src/main/java/rym/study/rckit/utils/MessageReceiver.java
@@ -0,0 +1,19 @@
+package rym.study.rckit.utils;
+
+import android.content.Context;
+
+import io.rong.push.notification.PushMessageReceiver;
+import io.rong.push.notification.PushNotificationMessage;
+
+public class MessageReceiver extends PushMessageReceiver {
+
+    @Override
+    public boolean onNotificationMessageArrived(Context context, PushNotificationMessage message) {
+        return false;
+    }
+
+    @Override
+    public boolean onNotificationMessageClicked(Context context, PushNotificationMessage message) {
+        return false;
+    }
+}
